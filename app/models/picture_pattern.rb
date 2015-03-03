a2a89c04c5f30517bc1d9d32fcebf0bd44e0675f
max-konin <mk@kernel-corp.com>
2015-03-03 11:17:09 +0600

    type integer

--- app/models/picture_pattern.rb
+++ app/models/picture_pattern.rb
@@ -3,7 +3,7 @@ class PicturePattern
 
   field :title
   field :description
-  field :picture_number
+  field :picture_number, type: Integer
 
   scope :for_question, -> (number) {where picture_number: number}
 end
